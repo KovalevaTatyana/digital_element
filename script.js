82e067b9092119719e84d61ca48c7a21f2320f66
Kovaleva Tatyana <kovaleva.tatyana@protonmail.com>
2021-10-23 18:52:20 +0500

    fix validate and add popup

--- script.js
+++ script.js
@@ -18,17 +18,41 @@ function validate() {
     let email = document.getElementById("email");
     let message = document.getElementById("message");
 
+    name.style.border= null;
+    email.style.border = null;
+    message.style.border = null;
+
+    let result = true;
     if(!name.value) {
         name.style.border = "2px solid red";
-        return false;
+        result = false;
     }
     if(!email.value) {
         email.style.border = "2px solid red";
-        return false;
+        result = false;
     }
     if(!message.value) {
         message.style.border = "2px solid red";
-        return false;
+        result = false;
     }
-    return true;
+    return result;
+}
+let popup = document.getElementById("popup");
+let button = document.getElementById("modal-form-btn");
+
+button.onclick = function(event) {
+    event.preventDefault();
+    fetch('example.com', {
+        method: 'POST',
+    }).then(function(response){
+    }).catch(function(error){
+    });
+    if (validate()) {
+        popup.style.display = "block";
+    }
+}
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
 }
